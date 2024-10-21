52a32b826aef0ff51247e15d5c7edf375571493e
Kei-K23 <keiksl2301@gmail.com>
2024-10-21 12:28:37 +0630

    Update routing system to support HTTP methods

--- src/main/java/com/keik23/router/Router.java
+++ src/main/java/com/keik23/router/Router.java
@@ -2,37 +2,36 @@ package com.keik23.router;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.http.HttpRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import com.keik23.http.Request;
 
 public class Router {
-    private Map<String, Consumer<Request>> routes = new HashMap<>();
+    private Map<String, Map<String, BiConsumer<Request, OutputStream>>> routes = new HashMap<>();
 
-    public void addRoutes(String path, Consumer<Request> handler) {
-        routes.put(path, handler);
+    public void addRoute(String method, String path, BiConsumer<Request, OutputStream> handler) {
+        routes.computeIfAbsent(method, k -> new HashMap<>()).put(path, handler);
     }
 
     public void requestHandler(Request request, OutputStream output) throws IOException {
-        var handler = routes.get(request.getPath());
-        if (handler != null) {
-            handler.accept(request);
+        var methodRoutes = routes.get(request.getMethod());
 
-            String httpResponse = "HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: text/plain\r\n" +
-                    "Content-Length: 13\r\n" +
-                    "\r\n" +
-                    "Hello, world!";
-            output.write(httpResponse.getBytes());
-        } else {
-            String httpResponse = "HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: text/plain\r\n" +
-                    "Content-Length: 13\r\n" +
-                    "\r\n" +
-                    "Not found!";
-            output.write(httpResponse.getBytes());
+        if (methodRoutes != null) {
+            var handler = methodRoutes.get(request.getPath());
+            if (handler != null) {
+                handler.accept(request, output);
+                return;
+            }
         }
+        String httpResponse = "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: 13\r\n" +
+                "\r\n" +
+                "Not found!";
+        output.write(httpResponse.getBytes());
     }
 }
